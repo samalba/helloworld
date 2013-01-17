69fc9c3683e688950eda5c13030104355247953d
Sam Alba <sam.alba@gmail.com>
2013-01-16 16:52:55 -0800

    Added /env for debug

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 
 import os
+import flask
 from flask import Flask
 
 
 app = Flask(__name__)
+app.debug = True
+
+
+def response(data, code=200):
+    headers = {
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Expires': '-1',
+            'Content-Type': 'text/plain'
+            }
+    if not isinstance(data, basestring):
+        try:
+            data = flask.json.dumps(data, indent=4, sort_keys=True, skipkeys=True)
+            headers['Content-type'] = 'application/json'
+        except TypeError:
+            data = str(data)
+    return app.make_response((data, code, headers))
 
 
 @app.route('/')
@@ -15,6 +33,11 @@ def hello():
 @app.route('/simple')
 def simple():
     return 'ok'
+
+
+@app.route('/env')
+def environ():
+    return response(dict(os.environ))
 
 
 if __name__ == '__main__':
