e1aa0f3f454ecab1f7298148ea8860ff9dc24ab3
Sam Alba <sam.alba@gmail.com>
2013-01-16 16:03:10 -0800

    Added env variable to detect heroku env

--- wsgi.py
+++ wsgi.py
@@ -21,3 +21,6 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+else:
+    # For uwsgi
+    application = app
